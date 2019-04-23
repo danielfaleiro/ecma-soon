d1850a4fdb2e59cc48196b444ca47368d8d338ea
Daniel <danielfaleirosilva@gmail.com>
2019-04-23 11:40:07 -0300

    Updates

---
+++ precache-manifest.4301dfea4c5ff71c7bf4c7d57ca4cf65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6730a7d9457a61bf8cf17e5d94b21fb5",
+    "url": "/ecma-soon/index.html"
+  },
+  {
+    "revision": "e08f4ba0fcf92b566ea3",
+    "url": "/ecma-soon/static/css/main.ab964b26.chunk.css"
+  },
+  {
+    "revision": "b3d4c83cd42425f21e14",
+    "url": "/ecma-soon/static/js/2.046c266d.chunk.js"
+  },
+  {
+    "revision": "e08f4ba0fcf92b566ea3",
+    "url": "/ecma-soon/static/js/main.30359fb5.chunk.js"
+  },
+  {
+    "revision": "54d659e49eb00a487da0",
+    "url": "/ecma-soon/static/js/runtime~main.a9f6d2c9.js"
+  },
+  {
+    "revision": "027705bfe7584463dbffa13a6216a570",
+    "url": "/ecma-soon/static/media/ECMA - gold.027705bf.png"
+  }
+]);
