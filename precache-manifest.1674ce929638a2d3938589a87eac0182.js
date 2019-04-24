abf049e1e6de473cacfd53be1fefa5f747290f4e
Daniel <danielfaleirosilva@gmail.com>
2019-04-24 00:58:40 -0300

    Updates

---
+++ precache-manifest.1674ce929638a2d3938589a87eac0182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d148f86f1ca4bb5481a588f675a0c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d97bdf7874f151dfd56",
+    "url": "/static/css/main.0b3ee01b.chunk.css"
+  },
+  {
+    "revision": "724c895990e894920842",
+    "url": "/static/js/2.6c35676d.chunk.js"
+  },
+  {
+    "revision": "9d97bdf7874f151dfd56",
+    "url": "/static/js/main.d39f9d69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "027705bfe7584463dbffa13a6216a570",
+    "url": "/static/media/ECMA - gold.027705bf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/ecma-soon/precache-manifest.611e382fea573980dbbc0b877cd698d8.js"
+  "/precache-manifest.1674ce929638a2d3938589a87eac0182.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ecma-soon/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
